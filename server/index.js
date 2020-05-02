cd6eef3774deff95d55547156b72c8e7ba02f0ff
Kayla <golderkayla@gmail.com>
2020-05-01 20:10:13 -0700

    seeding the db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-console */
 const express = require('express');
+const path = require('path');
+const db = require('../database');
 
 const app = express();
+const PORT = 4007;
 
-const PORT = 3000;
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.get('/:listingId', (req, res) => {
+  db.Review.find({ listingId: req.params.listingId })
+    .then((data) => res.send(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
