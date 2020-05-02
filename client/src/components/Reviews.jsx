948eaa0efc30b557a3586d1722ca866c1ef0cb4f
Kayla <golderkayla@gmail.com>
2020-05-02 14:02:40 -0700

    added unit test for app

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -9,7 +9,7 @@ class Reviews extends React.Component {
 
   render() {
     return (
-      <div>hello</div>
+      <div>hello there</div>
     );
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Reviews from './components/Reviews.jsx';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <div>hello</div>
-    );
-  }
-}
+import App from './components/App';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
