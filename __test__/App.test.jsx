a49ab899dee56da1f78c7b4a346c7b717a37729b
Kayla <golderkayla@gmail.com>
2020-05-08 21:51:42 -0700

    clean up code

--- __test__/App.test.jsx
+++ __test__/App.test.jsx
@@ -1,25 +1,12 @@
 /* eslint-env jest */
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-// import axios from 'axios';
+import { shallow } from 'enzyme';
+
 import App from '../client/src/components/App';
-import Review from '../client/src/components/Review';
-import RatingLevels from '../client/src/components/RatingLevels';
-
-import reviews from './mockReviews';
-
 
 describe('<App/>', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
-
   it('should render the App component on the screen', () => {
-    // const wrapper = shallow(<App />);
+    const wrapper = shallow(<App />);
     expect(wrapper).toExist();
   });
-
-
-
 });
